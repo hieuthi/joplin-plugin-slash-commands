80ccd291b90858d16fafe543724f9decfe22f3cb
Luong Hieu Thi <luonghieuthi@gmail.com>
2022-01-15 12:27:06 +0900

    Update default definition

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import joplin from 'api';
 import { ContentScriptType } from 'api/types';
 import { SettingItemType } from 'api/types';
 
-const DEFAULT_DEFINITIONS = '[["datetime", "now", [ "dd/mm/yyyy HH:MM", "yyyy-mm-dd\\"T\\"HH:MM:ss" ] ], ["datetime", "date", [ "dd/mm/yyyy", "yyyy-mm-dd" ] ], ["datetime", "time", [ "HH:MM", "HH:MM:ss" ] ], ["string", "greet", [ "Hello World" ] ], ["string", "task", [ "- [ ] " ] ], ["string", "todotxt", [ "```todotxt sort:default\\n\\n```" ] ], ["datetime", "todoa", [ "\\"(A)\\" yyyy-mm-dd " ] ], ["datetime", "todob", [ "\\"(B)\\" yyyy-mm-dd " ] ], ["datetime", "todoc", [ "\\"(C)\\" yyyy-mm-dd " ] ], ["datetime", "todod", [ "\\"(D)\\" yyyy-mm-dd " ] ], ["datetime", "todoe", [ "\\"(E)\\" yyyy-mm-dd " ] ], ["calendar", "calendar", [null,"en-US","ja-JP"] ] ]';
+const DEFAULT_DEFINITIONS = '[["datetime", "now", [ "dd/mm/yyyy HH:MM", "yyyy-mm-dd\\"T\\"HH:MM:ss" ] ], ["datetime", "date", [ "dd/mm/yyyy", "yyyy-mm-dd" ] ], ["datetime", "time", [ "HH:MM", "HH:MM:ss" ] ], ["string", "greet", [ "Hello", "你好", "Bonjour", "Hallo", "नमस्ते", "こんにちは", "안녕하세요", "Hola", "Xin chào" ] ], ["string", "task", [ "- [ ] " ] ], ["string", "todotxt", [ "```todotxt sort:default\\n\\n```" ] ], ["datetime", "todoa", [ "\\"(A)\\" yyyy-mm-dd " ] ], ["datetime", "todob", [ "\\"(B)\\" yyyy-mm-dd " ] ], ["datetime", "todoc", [ "\\"(C)\\" yyyy-mm-dd " ] ], ["datetime", "todod", [ "\\"(D)\\" yyyy-mm-dd " ] ], ["datetime", "todoe", [ "\\"(E)\\" yyyy-mm-dd " ] ], ["calendar", "calendar", ["en-us"] ], ["calendar", "calendar", [null,"en-US","ja-JP"] ] ]';
 
 joplin.plugins.register({
   onStart: async function() {
